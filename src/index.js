ac34d91e051703c9ac9eac21451264e4c4276672
emilyrh <114513651+emilyrh@users.noreply.github.com>
2024-10-28 17:18:55 +0000

    Add basic search engine behaviour

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElememt = document.querySelector("#city");
+  cityElememt.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
